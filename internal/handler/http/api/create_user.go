e2b3b232f5ae2a9abbcfd0594e8eda4029233a90
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-03-13 15:08:37 +0300

    add user create handler

---
+++ internal/handler/http/api/create_user.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+	"test-api-task/internal/entity"
+	"test-api-task/internal/service/userservice"
+
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	const op = "handler.http.api.create_user"
+
+	log := h.log.With(
+		slog.String("op", op),
+		slog.String("request_id", middleware.GetReqID(r.Context())),
+	)
+
+	var newUser entity.User
+	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
+		log.Error("failed to decode response", slog.Any("error", err.Error()))
+		http.Error(w, "invalid request", http.StatusNotFound)
+
+		return
+	}
+
+	createdUser, err := h.us.CreateUser(&newUser)
+	if err != nil {
+		if errors.Is(err, userservice.ErrEmailAlreadyExists) {
+			log.Info("couldn't create a user because the email is already busy", slog.Any("user_info", newUser))
+			http.Error(w, "bad request", http.StatusBadRequest)
+
+			return
+		}
+
+		log.Error("failed to create user", slog.Any("user_info", newUser), slog.Any("error", err.Error()))
+		http.Error(w, "internal error", http.StatusInternalServerError)
+
+		return
+	}
+
+	log.Info("user created", slog.Any("created_user", createdUser))
+
+	resp, err := json.MarshalIndent(createdUser, "", " ")
+	if err != nil {
+		log.Error("failed to marshal response", slog.Any("error", err.Error()))
+		http.Error(w, "internal error", http.StatusInternalServerError)
+
+		return
+	}
+
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Error("failed to write response", slog.Any("error", err.Error()))
+		http.Error(w, "internal error", http.StatusInternalServerError)
+
+		return
+	}
+}
--- internal/handler/http/api/router.go
+++ internal/handler/http/api/router.go
@@ -4,4 +4,5 @@ import "github.com/go-chi/chi/v5"
 
 func (h *Handler) AddRoutes(r chi.Router) {
 	r.Get("/users/{id}", h.GetUser)
+	r.Post("/users", h.CreateUser)
 }
